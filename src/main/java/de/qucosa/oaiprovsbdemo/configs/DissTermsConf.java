4688a213b1aab3511a1f642d1e8ddad372404826
dseelig <daniel.seelig@slub-dresden.de>
2018-07-09 14:33:25 +0200

    add simple example conf

--- src/main/java/de/qucosa/oaiprovsbdemo/configs/DissTermsConf.java
+++ src/main/java/de/qucosa/oaiprovsbdemo/configs/DissTermsConf.java
@@ -1,4 +1,17 @@
 package de.qucosa.oaiprovsbdemo.configs;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class DissTermsConf {
+
+    private String arg;
+
+    public DissTermsConf(String arg) {
+        this.arg = arg;
+    }
+
+    public String blabla() {
+        return arg;
+    }
 }
