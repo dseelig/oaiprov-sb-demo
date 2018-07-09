697ea061331d3335637a3a6452e67ef3a059c1bd
dseelig <daniel.seelig@slub-dresden.de>
2018-07-09 14:43:58 +0200

    Add autowire environment and use property

--- src/main/java/de/qucosa/oaiprovsbdemo/configs/AppConf.java
+++ src/main/java/de/qucosa/oaiprovsbdemo/configs/AppConf.java
@@ -1,13 +1,18 @@
 package de.qucosa.oaiprovsbdemo.configs;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 
 @Configuration
 public class AppConf {
 
+    @Autowired
+    private Environment environment;
+
     @Bean
     public DissTermsConf dissTermsConf() {
-        return new DissTermsConf("blub blub");
+        return new DissTermsConf(environment.getProperty("db.type"));
     }
 }
