2890c2e472906c20df587ed6e3e2b40fe5419a49
dseelig <daniel.seelig@slub-dresden.de>
2018-07-09 14:34:39 +0200

    Add example diss terms conf instance.

--- src/main/java/de/qucosa/oaiprovsbdemo/configs/AppConf.java
+++ src/main/java/de/qucosa/oaiprovsbdemo/configs/AppConf.java
@@ -1,4 +1,13 @@
 package de.qucosa.oaiprovsbdemo.configs;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class AppConf {
+
+    @Bean
+    public DissTermsConf dissTermsConf() {
+        return new DissTermsConf("blub blub");
+    }
 }
